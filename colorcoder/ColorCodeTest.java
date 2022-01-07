23b7f7f3996ecf8cf8c7a778e7ca3a967bf3abd9
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-01-07 16:05:05 +0530

    Update ColorCodeTest.java

--- colorcoder/ColorCodeTest.java
+++ colorcoder/ColorCodeTest.java
@@ -1,21 +1,14 @@
 package colorcoder;
 
 public class ColorCoderTest {
-static void testNumberToPair(int pairNumber,
-        MajorColor expectedMajor,
-        MinorColor expectedMinor)
-    {
+static void testNumberToPair(int pairNumber,MajorColor expectedMajor,MinorColor expectedMinor){
         ColorPair colorPair = GetColorFromPairNumber(pairNumber);
         System.out.println("Got pair " + colorPair.ToString());
         assert(colorPair.getMajor() == expectedMajor);
         assert(colorPair.getMinor() == expectedMinor);
     }
 
-    static void testPairToNumber(
-        MajorColor major,
-        MinorColor minor,
-        int expectedPairNumber)
-    {
+static void testPairToNumber(MajorColor major,MinorColor minor,int expectedPairNumber){
         int pairNumber = GetPairNumberFromColor(major, minor);
         System.out.println("Got pair number " + pairNumber);
         assert(pairNumber == expectedPairNumber);
