7a7349c2b3776839b209278258639c80b9f2c6e3
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-01-07 16:49:30 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -2,14 +2,6 @@ package colorcoder;
 
 public class Main {
 
-    final static String MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-    };
-    final static int numberOfMajorColors = MajorColorNames.length;
-    final static String MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"
-    };
-    final static int numberOfMinorColors = MinorColorNames.length;
 
     public static void main(String[] args) {
         ColorCoderTest.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
