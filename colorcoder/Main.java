e7c613a7788780fab9fe445c006bfbd8ccf43b8b
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-01-07 16:37:20 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -11,20 +11,6 @@ public class Main {
     };
     final static int numberOfMinorColors = MinorColorNames.length;
 
-    static ColorPair GetColorFromPairNumber(int pairNumber) {
-        int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = 
-            MajorColor.fromIndex(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor =
-            MinorColor.fromIndex(zeroBasedPairNumber % numberOfMinorColors);
-        return new ColorPair(majorColor, minorColor);
-    }
-    static int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major.getIndex() * numberOfMinorColors + minor.getIndex() + 1;
-    }
-
-
-
     public static void main(String[] args) {
         ColorCoderTest.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
         ColorCoderTest.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
